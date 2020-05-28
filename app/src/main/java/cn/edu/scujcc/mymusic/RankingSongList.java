806f116fc6fbff4272ddea71dd4cb611a76c3e30
Natsu <1101693441@qq.com>
2020-05-28 16:36:28 +0800

    "更改榜单图片名称2"

---
+++ app/src/main/java/cn/edu/scujcc/mymusic/RankingSongList.java
@@ -0,0 +1,8 @@
+package cn.edu.scujcc.mymusic;
+
+public class RankingSongList {
+    private String artist_id;
+    private String original_song;
+    private String hot_song;
+    private String new_song;
+}
