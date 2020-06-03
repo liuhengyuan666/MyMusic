284e1dcb1b12363af6236decbae499f2c77c9db3
DING <562113724@qq.com>
2020-06-03 16:10:52 +0800

    点击注册跳转到注册界面，点击登录跳转到主界面。

--- app/src/main/java/cn/edu/scujcc/mymusic/LoginActivity.java
+++ app/src/main/java/cn/edu/scujcc/mymusic/LoginActivity.java
@@ -2,14 +2,43 @@ package cn.edu.scujcc.mymusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private Button register_button;
+    private Button login_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register_button= (Button) findViewById(R.id.register_button);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        login_button= (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
