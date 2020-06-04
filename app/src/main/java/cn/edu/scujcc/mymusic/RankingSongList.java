b207bc33a92eac69c8c5cfb59252031770358c24
Natsu <1101693441@qq.com>
2020-06-04 17:48:17 +0800

    "修改榜单从网上读取数据"

--- app/src/main/java/cn/edu/scujcc/mymusic/RankingSongList.java
+++ app/src/main/java/cn/edu/scujcc/mymusic/RankingSongList.java
@@ -5,123 +5,149 @@ import java.util.List;
 import java.util.Objects;
 
 public class RankingSongList {
-    private String singer_id;
-    private String original_song;
-    private String hot_song;
-    private String new_song;
-    private String songid;
-    private String songname;
-    private String singer;
-    private String quality;
-    private String url;
-    //private List<SongList> songLists;
+    String billboard_type;//billboard_type": "1",
+    String billboard_no;//"billboard_no": "3268",
+    String update_date;//  "update_date": "2020-06-05",
+    String billboard_songnum;//"billboard_songnum": "100",
+    int havemore;// "havemore": 1,
+    String name;// "name": "新歌榜",
+    String comment;// "comment": "该榜单是根据动听音乐平台歌曲每日播放量自动生成的数据榜单，统计范围为近期发行的歌曲，每日更新一次",
+    String pic_s192;// "pic_s192": "http://business.cdn.qianqian.com/qianqian/pic/bos_client_9a4fbbbfa50203aaa9e69bf189c6a45b.jpg",
+    String pic_s640;// "pic_s640": "http://business.cdn.qianqian.com/qianqian/pic/bos_client_a4aa99cf8bf218304de9786b6ba38982.jpg",
+    String pic_s444;//"pic_s444": "http://hiphotos.qianqian.com/ting/pic/item/78310a55b319ebc4845c84eb8026cffc1e17169f.jpg",
+    String pic_s260;// "pic_s260": "http://hiphotos.qianqian.com/ting/pic/item/e850352ac65c1038cb0f3cb0b0119313b07e894b.jpg",
+    String pic_s210;// "pic_s210": "http://business.cdn.qianqian.com/qianqian/pic/bos_client_dea655f4be544132fb0b5899f063d82e.jpg",
+    String web_url;//  "web_url": "http://music.baidu.com/top/new",
+    String color;//"color": "0x5B9400",
+    String bg_color;// "bg_color": "0xEFF5E6",
+    String bg_pic;// "bg_pic": "http://business0.qianqian.com/qianqian/file/5bfe504b1c299_491.png"
 
-
-    public String getSinger_id() {
-        return singer_id;
+    public String getBillboard_type() {
+        return billboard_type;
     }
 
-    public void setSinger_id(String singer_id) {
-        this.singer_id = singer_id;
+    public void setBillboard_type(String billboard_type) {
+        this.billboard_type = billboard_type;
     }
 
-    public String getOriginal_song() {
-        return original_song;
+    public String getBillboard_no() {
+        return billboard_no;
     }
 
-    public void setOriginal_song(String original_song) {
-        this.original_song = original_song;
+    public void setBillboard_no(String billboard_no) {
+        this.billboard_no = billboard_no;
     }
 
-    public String getHot_song() {
-        return hot_song;
+    public String getUpdate_date() {
+        return update_date;
     }
 
-    public void setHot_song(String hot_song) {
-        this.hot_song = hot_song;
+    public void setUpdate_date(String update_date) {
+        this.update_date = update_date;
     }
 
-    public String getNew_song() {
-        return new_song;
+    public String getBillboard_songnum() {
+        return billboard_songnum;
     }
 
-    public void setNew_song(String new_song) {
-        this.new_song = new_song;
+    public void setBillboard_songnum(String billboard_songnum) {
+        this.billboard_songnum = billboard_songnum;
     }
 
-    public String getSongid() {
-        return songid;
+    public int getHavemore() {
+        return havemore;
     }
 
-    public void setSongid(String songid) {
-        this.songid = songid;
+    public void setHavemore(int havemore) {
+        this.havemore = havemore;
     }
 
-    public String getSongname() {
-        return songname;
+    public String getName() {
+        return name;
     }
 
-    public void setSongname(String songname) {
-        this.songname = songname;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getSinger() {
-        return singer;
+    public String getComment() {
+        return comment;
     }
 
-    public void setSinger(String singer) {
-        this.singer = singer;
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
-    public String getQuality() {
-        return quality;
+    public String getPic_s192() {
+        return pic_s192;
     }
 
-    public void setQuality(String quality) {
-        this.quality = quality;
+    public void setPic_s192(String pic_s192) {
+        this.pic_s192 = pic_s192;
     }
 
-    public String getUrl() {
-        return url;
+    public String getPic_s640() {
+        return pic_s640;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public void setPic_s640(String pic_s640) {
+        this.pic_s640 = pic_s640;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RankingSongList that = (RankingSongList) o;
-        return singer_id.equals(that.singer_id) &&
-                original_song.equals(that.original_song) &&
-                hot_song.equals(that.hot_song) &&
-                new_song.equals(that.new_song) &&
-                songid.equals(that.songid) &&
-                songname.equals(that.songname) &&
-                singer.equals(that.singer) &&
-                quality.equals(that.quality) &&
-                url.equals(that.url);
+    public String getPic_s444() {
+        return pic_s444;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(singer_id, original_song, hot_song, new_song, songid, songname, singer, quality, url);
+    public void setPic_s444(String pic_s444) {
+        this.pic_s444 = pic_s444;
     }
 
-    @Override
-    public String toString() {
-        return "RankingSongList{" +
-                "singer_id='" + singer_id + '\'' +
-                ", original_song='" + original_song + '\'' +
-                ", hot_song='" + hot_song + '\'' +
-                ", new_song='" + new_song + '\'' +
-                ", songid='" + songid + '\'' +
-                ", songname='" + songname + '\'' +
-                ", singer='" + singer + '\'' +
-                ", quality='" + quality + '\'' +
-                ", url='" + url + '\'' +
-                '}';
+    public String getPic_s260() {
+        return pic_s260;
     }
+
+    public void setPic_s260(String pic_s260) {
+        this.pic_s260 = pic_s260;
+    }
+
+    public String getPic_s210() {
+        return pic_s210;
+    }
+
+    public void setPic_s210(String pic_s210) {
+        this.pic_s210 = pic_s210;
+    }
+
+    public String getWeb_url() {
+        return web_url;
+    }
+
+    public void setWeb_url(String web_url) {
+        this.web_url = web_url;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBg_color() {
+        return bg_color;
+    }
+
+    public void setBg_color(String bg_color) {
+        this.bg_color = bg_color;
+    }
+
+    public String getBg_pic() {
+        return bg_pic;
+    }
+
+    public void setBg_pic(String bg_pic) {
+        this.bg_pic = bg_pic;
+    }
+
 }
