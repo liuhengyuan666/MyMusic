43b8f0befca6daf3dac94a1e8b7c28ded81110c5
Natsu <1101693441@qq.com>
2020-06-04 17:15:00 +0800

    "修改主页界面及榜单页界面"

--- app/src/main/java/cn/edu/scujcc/mymusic/RankingList.java
+++ app/src/main/java/cn/edu/scujcc/mymusic/RankingList.java
@@ -2,15 +2,20 @@ package cn.edu.scujcc.mymusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RankingList extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking_list);
-    }
 
 
+}
 }
