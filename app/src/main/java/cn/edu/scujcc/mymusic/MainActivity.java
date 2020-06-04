9aeb21f5665b9979279795b03b60b3751a0e7f92
本地音乐 <907286155@qq.com>
2020-06-04 17:04:14 +0800

    歌曲信息/本地音乐界面重

--- app/src/main/java/cn/edu/scujcc/mymusic/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/mymusic/MainActivity.java
@@ -4,24 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initView();
-
-        int  REQUEST_EXTERNAL_STORAGE = 1;
-        String[] PERMISSIONS_STORAGE ={
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-
-    }
-
-    private void initView() {
+        gridView=findViewById(R.id.gridview);
+        gridView.setAdapter();
     }
 }
